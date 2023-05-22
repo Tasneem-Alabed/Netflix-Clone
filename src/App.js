2269868a83c040a45cc667e0711ec8eda018d32b
Tasneem-Alabed <tsneemabedd@gmail.com>
2023-05-22 18:04:02 +0000

    final version

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import Home from "./componants/home/Home";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Favorit from "./componants/FavList/Favlist"
 import { Routes , Route } from "react-router-dom";
+import axios from "axios";
+
+import { useEffect, useState } from 'react'
 function App() {
+  const [trendData, setTrendData] = useState([]);
+
+  const getTrending = () => {
+     const serverURL = `http://localhost:3001/trending`;
+
+
+  axios.get(serverURL)
+  .then(response => {
+      
+          console.log(response.data)
+          setTrendData(response.data)
+
+      })
+  
+  }
+  useEffect(()=>
+   getTrending() ,[]
+  );
   return (
    <>
    
    <Routes>
-    <Route path="/" element={<Home/>}></Route>
-    <Route path="/Favorit" element={<Favorit/>}></Route>
+    <Route path="/" element={<Home data={trendData}/>}></Route>
+    <Route path="/Favorit" element={<Favorit data={trendData} />}></Route>
    </Routes>
    </>
   );
--- src/componants/home/Home.js
+++ src/componants/home/Home.js
@@ -1,35 +1,16 @@
 import MoviList from "../MovieList/MovieList";
-import axios from "axios";
 import Navgbar from "../Navbar/NavBar";
-import { useEffect, useState } from 'react'
 // import Card from 'react-bootstrap/Card';
 
-function Home(){
-    const [trendData, setTrendData] = useState([]);
-
-    const getTrending = () => {
-       const serverURL = `http://localhost:3001/trending`;
-
-
-    axios.get(serverURL)
-    .then(response => {
-        
-            console.log(response.data)
-            setTrendData(response.data)
-
-        })
-    
-    }
-    useEffect(()=>
-     getTrending() ,[]
-    );
+function Home(props){
+  
     return(<>
    
     <Navgbar/>
  
 
-<p>MoviList</p>
-     <MoviList data={trendData}/>
+<h1>MoviList</h1>
+     <MoviList data={props.data} />
     </>)
 };
 
